62cdc46e768af048237b2810031ebdff9fa55993
LJZN <rubyminer@foxmail.com>
2017-05-24 22:44:31 +0800

    fun_safe_register_cluster_info

--- lib/rialix_core/application.ex
+++ lib/rialix_core/application.ex
@@ -8,12 +8,12 @@ defmodule RialixCore.Application do
 
   """
   def start(_start_type, _start_args) do
-    maybe_delay_start()
-    :ok = validate_ring_state_directory_exists()
-    :ok = safe_register_cluster_info()
-    :ok = add_bucket_deafults()
+    maybe_delay_start
+    :ok = validate_ring_state_directory_exists
+    :ok = safe_register_cluster_info
+    :ok = add_bucket_deafults
 
-    start_riak_core_sup()
+    start_rialix_core_sup
   end
 
   @doc """
@@ -23,18 +23,46 @@ defmodule RialixCore.Application do
     Logger.info "Stopped application rialix_core."
     :ok
   end
-<<<<<<< HEAD:lib/rialix_core/app.ex
 
   defp maybe_delay_start() do
     case Application.get_env :rialix_core, :delayed_start do
-      {:ok, delay} ->
+      nil ->
+        :ok
+      delay ->
         Logger.info "Delaying rialix_core startup as requested"
         :timer.sleep delay
-      _ ->
-        :ok
     end
   end
 
-=======
->>>>>>> 900a855ec72b4a07e58a7e57c5e6bd6d594b97f9:lib/rialix_core/application.ex
+  defp safe_register_cluster_info do
+    ClusterInfo.register_app :rialix_core_cinfo_core
+  catch
+    _, _ ->
+      :ok
+  end
+
+  defp add_bucket_defaults do
+    :default_type
+    |> RialixCore.Bucket.Type.defaults()
+    |> RialixCore.Bucket.append_bucket_defaults()
+
+    :ok
+  end
+
+  defp start_rialix_core_sup do
+    case RialixCore.Supervisor.start_link do
+      {:ok, pid} ->
+        :ok = register_applications
+        :ok = add_ring_event_handler
+
+        :ok = register_capabilities
+        :ok = init_cli_registry
+        :ok = RialixCore.Throttle.init
+
+        {:ok, pid}
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
 end
