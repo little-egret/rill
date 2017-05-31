a8a8ea074b68a792c1a4a4647ae7e1c100d4effd
LJZN <rubyminer@foxmail.com>
2017-05-31 22:49:27 +0800

    fun-ring_trans

--- apps/rill/lib/rill/ring/manager.ex
+++ apps/rill/lib/rill/ring/manager.ex
@@ -28,4 +28,34 @@ defmodule Rill.Ring.Manager do
     end
     run_fixups(t, bucket_name, bp)
   end
+
+  @doc """
+
+  """
+  def ring_trans(fun, args) do
+    GenServer.call(__MODULE__, {:ring_trans, fun, args}, :infinity)
+  end
+
+  def handle_call({:ring_trans, fun, args}, _, state=%state{raw_ring: ring}) do
+    case fun.(ring, args) do
+      {:new_ring, new_ring} ->
+        state2 = prune_write_notify_ring(new_ring, state)
+        Rill.Gossip.random_recursive_gossip(new_ring)
+        {:reply, {:ok, new_ring}, state2}
+      {set_only, new_ring} ->
+        state2 = prune_write_ring(new_ring, state)
+        {:reply, {:ok, new_ring}, state2}
+      {:reconciled_ring, new_ring} ->
+        state2 = prune_write_notify_ring(new_ring, state)
+        Rill.Gossip.recursive_gossip(new_ring)
+        {:reply, {:ok, new_ring}, state2}
+      :ignore ->
+        {:reply, :not_changed, state}
+      {:ignore, reason} ->
+        {:reply, {:not_changed, reason}, state}
+      other ->
+        Logger.error("ring_trans: invalid return value: #{inspect other}")
+        {:reply, :not_changed, state}
+    end
+  end
 end
